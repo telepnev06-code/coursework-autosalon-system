b0ea3ec2633857fa570132cd0ce0c10f9d0806e2
telepnev06-code <telepnev06@gmail.com>
2026-04-15 00:32:52 +0300

    feat: configure application entry point for GUI mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#using <System.dll>
+#using <System.Windows.Forms.dll>
+#include "MyForm.h"
 
-int main() {
-    std::cout << "Autosalon System Initialized!" << std::endl;
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    AutosalonApp::MyForm^ form = gcnew AutosalonApp::MyForm();
+    Application::Run(form);
     return 0;
 }
