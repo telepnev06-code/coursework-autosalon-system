3b0c2fe31d2b9bdfeb048b0a165e6e97ecd2ed60
telepnev06-code <telepnev06@gmail.com>
2026-04-15 00:50:05 +0300

    feat: add UI elements for car management

--- src/MyForm.h
+++ src/MyForm.h
@@ -13,12 +13,28 @@ namespace AutosalonApp {
     private:
         Label^ systemLabel;
         DataGridView^ carDataGridView;
-        Button^ addTestCarButton;
+        
+        // Поля ввода
+        Label^ brandLabel;
+        TextBox^ brandTextBox;
+        
+        Label^ modelLabel;
+        TextBox^ modelTextBox;
+        
+        Label^ yearLabel;
+        TextBox^ yearTextBox;
+        
+        Label^ priceLabel;
+        TextBox^ priceTextBox;
+
+        // Кнопки
+        Button^ addButton;
+        Button^ deleteButton;
 
     public:
         MyForm() {
             this->Text = L"Автосалон";
-            this->Size = System::Drawing::Size(600, 400);
+            this->Size = System::Drawing::Size(600, 450);
 
             systemLabel = gcnew Label();
             systemLabel->Text = L"Система управления автосалоном";
@@ -27,28 +43,87 @@ namespace AutosalonApp {
 
             carDataGridView = gcnew DataGridView();
             carDataGridView->Location = Point(10, 40);
-            carDataGridView->Size = System::Drawing::Size(560, 250);
+            carDataGridView->Size = System::Drawing::Size(560, 240);
             carDataGridView->ColumnCount = 4;
             carDataGridView->Columns[0]->Name = L"Марка";
             carDataGridView->Columns[1]->Name = L"Модель";
             carDataGridView->Columns[2]->Name = L"Год";
             carDataGridView->Columns[3]->Name = L"Цена";
 
-            addTestCarButton = gcnew Button();
-            addTestCarButton->Text = L"Добавить тестовое авто";
-            addTestCarButton->Location = Point(10, 300);
-            addTestCarButton->Size = System::Drawing::Size(200, 40);
-            addTestCarButton->Click += gcnew EventHandler(this, &MyForm::OnAddTestCarClick);
+            // Марка
+            brandLabel = gcnew Label();
+            brandLabel->Text = L"Марка:";
+            brandLabel->Location = Point(10, 290);
+            brandLabel->AutoSize = true;
+            
+            brandTextBox = gcnew TextBox();
+            brandTextBox->Location = Point(10, 310);
+            brandTextBox->Width = 120;
 
+            // Модель
+            modelLabel = gcnew Label();
+            modelLabel->Text = L"Модель:";
+            modelLabel->Location = Point(140, 290);
+            modelLabel->AutoSize = true;
+
+            modelTextBox = gcnew TextBox();
+            modelTextBox->Location = Point(140, 310);
+            modelTextBox->Width = 120;
+
+            // Год
+            yearLabel = gcnew Label();
+            yearLabel->Text = L"Год:";
+            yearLabel->Location = Point(270, 290);
+            yearLabel->AutoSize = true;
+
+            yearTextBox = gcnew TextBox();
+            yearTextBox->Location = Point(270, 310);
+            yearTextBox->Width = 80;
+
+            // Цена
+            priceLabel = gcnew Label();
+            priceLabel->Text = L"Цена:";
+            priceLabel->Location = Point(360, 290);
+            priceLabel->AutoSize = true;
+
+            priceTextBox = gcnew TextBox();
+            priceTextBox->Location = Point(360, 310);
+            priceTextBox->Width = 80;
+
+            // Кнопка Добавить
+            addButton = gcnew Button();
+            addButton->Text = L"Добавить";
+            addButton->Location = Point(10, 350);
+            addButton->Size = System::Drawing::Size(120, 35);
+            addButton->Click += gcnew EventHandler(this, &MyForm::OnActionClick);
+
+            // Кнопка Удалить
+            deleteButton = gcnew Button();
+            deleteButton->Text = L"Удалить";
+            deleteButton->Location = Point(140, 350);
+            deleteButton->Size = System::Drawing::Size(120, 35);
+            deleteButton->Click += gcnew EventHandler(this, &MyForm::OnActionClick);
+
+            // Добавляем все элементы на форму
             this->Controls->Add(systemLabel);
             this->Controls->Add(carDataGridView);
-            this->Controls->Add(addTestCarButton);
+            
+            this->Controls->Add(brandLabel);
+            this->Controls->Add(brandTextBox);
+            this->Controls->Add(modelLabel);
+            this->Controls->Add(modelTextBox);
+            this->Controls->Add(yearLabel);
+            this->Controls->Add(yearTextBox);
+            this->Controls->Add(priceLabel);
+            this->Controls->Add(priceTextBox);
+            
+            this->Controls->Add(addButton);
+            this->Controls->Add(deleteButton);
         }
 
     private:
-        void OnAddTestCarClick(Object^ sender, EventArgs^ e) {
-            Car^ testCar = gcnew Car(L"Toyota", L"Camry", 2022, 25000.0, L"Отличное", L"2.5L, Автомат");
-            carDataGridView->Rows->Add(testCar->Brand, testCar->Model, testCar->Year, testCar->Price);
+        void OnActionClick(Object^ sender, EventArgs^ e) {
+            MessageBox::Show(L"Кнопка нажата!", L"Действие", MessageBoxButtons::OK, MessageBoxIcon::Information);
         }
     };
 }
