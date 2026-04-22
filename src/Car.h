95837e4af5cb96e0fcabd36453e0b98459b83fec
telepnev06-code <telepnev06@gmail.com>
2026-04-22 16:21:42 +0300

    feat: add market value, condition and specs properties

--- src/Car.h
+++ src/Car.h
@@ -9,14 +9,16 @@ namespace AutosalonApp {
         String^ Model;
         int Year;
         double Price;
+        double MarketValue;
         String^ Condition;
         String^ Specs;
 
-        Car(String^ brand, String^ model, int year, double price, String^ condition, String^ specs) {
+        Car(String^ brand, String^ model, int year, double price, double marketValue, String^ condition, String^ specs) {
             Brand = brand;
             Model = model;
             Year = year;
             Price = price;
+            MarketValue = marketValue;
             Condition = condition;
             Specs = specs;
         }
