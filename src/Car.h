987e257ebf5baa567de86c2a30780da8bec54bdc
telepnev06-code <telepnev06@gmail.com>
2026-05-15 23:21:43 +0300

    feat: define core car data model

---
+++ src/Car.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+using namespace System;
+
+namespace AutosalonApp {
+    public ref class Car {
+    public:
+        String^ Brand;
+        String^ Model;
+        int Year;
+        double Price;
+        double MarketValue;
+        String^ Condition;
+        String^ Specs;
+
+        Car(String^ brand, String^ model, int year, double price, double marketValue, String^ condition, String^ specs) {
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Price = price;
+            MarketValue = marketValue;
+            Condition = condition;
+            Specs = specs;
+        }
+    };
+}
